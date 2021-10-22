26e29b3f1ac7d001090828ab7489e21d13fc3276
kibet ishmael <66499851+kmishmael@users.noreply.github.com>
2021-10-22 10:50:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,4 @@
 
-app.get('/*', function(req,res){
-
-res.sendFile(path.join(__dirname, 'angular-build', 'index.html'))
-
-});
-
-// Start the app by listening on the default Heroku port
-
-app.listen(process.env.PORT || 8080);
-
-function requireHTTPS(req, res, next) {
-
-    // The 'x-forwarded-proto' check is for Heroku
-
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-
-        return res.redirect('https://' + req.get('host') + req.url);
-
-    }
 
 
 
